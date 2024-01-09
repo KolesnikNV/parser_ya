da6b2141745bf2e3c61a2021d4f6b53b14f63d62
Никита Колесник <nikitakolesnik@MacBook-Pro-Nikita.local>
2024-01-09 12:31:52 +0400

    f

--- link_parser.py
+++ link_parser.py
@@ -60,10 +60,7 @@ chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(
-    executable_path="/root/parser_ya/chromedriver-linux64",
-    options=chrome_options,
-)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
 
 class LinksCollector:
